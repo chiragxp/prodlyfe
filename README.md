d61dd8537d6ac64151116cd390ed6ec2f94ddd8c
chiragxp <95179516+chiragxp@users.noreply.github.com>
2022-12-15 21:14:57 -0500

    Few errors need to be fixed
    - Donation button

---
+++ README.md
@@ -0,0 +1,12 @@
+# PRODLYFE
+Producitivy Social Media platform using MERN Stack
+
+Web Development Group Project Made by-
+Section 1 - Group 10: Akshar Patel, Parthvi Shah, Chirag Parmar
+
+INSTRUCTIONS:
+npm install for client and server both
+npm start for client and server both
+
+npm install commands for client
+npm install --save --legacy-peer-deps
---
+++ server/models/postMessage.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const postSchema = mongoose.Schema({
+    title: { type: String, required:  true },
+    message: { type: String, required:  true },
+    name: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likes: { type: [String], default: [] },
+    comments: { type: [String], default: [] },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+
+export default PostMessage;
